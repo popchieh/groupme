f579a83297fbadc5d63b76cfe10938861ac68bc7
Evan <popchieh07@gmail.com>
2013-11-29 17:50:07 +0800

    ch1 finished

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Groupme::Application.config.secret_key_base = 'a3c747d0549f85def89884991aa6e4575b23d98159e9c2abea22afe1679f2eacd9b181e385527c36853305e734646a517378eafa39ec8c7ac45caa119ccc7863'
